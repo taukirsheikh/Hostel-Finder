97c8eb39f3b8ea2a271254d8c13f5f0931db63c6
taukirsheikh <taukirsheikh2@gmail.com>
2023-01-15 08:04:44 +0545

    404 page updated

--- src/Components/NoMatch.jsx
+++ src/Components/NoMatch.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import './style.css'
 
 export const NoMatch = () => {
   return (
     <>
-    wrong page landing
+    <div className="error">404</div>
+<br /><br />
+<span className="info">File not found</span>
+<img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" className="static" />
     </>
   )}
